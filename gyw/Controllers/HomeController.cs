d556cdaa190dc7505fc305f83c70b83c917ca955
stu <stu@qq.com>
2016-10-19 12:22:50 +0800

    列表

--- gyw/Controllers/HomeController.cs
+++ gyw/Controllers/HomeController.cs
@@ -26,5 +26,16 @@ namespace gyw.Controllers
 
             return View();
         }
+        public ActionResult geng()
+        {
+            string[] data = new string[] {"如何拯救被组织行乞的残疾人 ",
+                                          "评论 | 清白自信的官员从不会躲企业家 ",
+                                          "历史 | 八十年前的今天，鲁迅去世了 ",
+                                          "冯志明：冤案主导者的B面",
+                                          "谁来救济输在智商起跑线上的儿童", };
+            ViewBag.data = data;
+
+            return View();
+        }
     }
 }
