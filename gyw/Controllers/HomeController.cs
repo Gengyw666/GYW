7a86e991cb439bfe42586b8ec4fdfa0c0f2f5ebc
stu <stu@qq.com>
2016-11-02 12:05:05 +0800

    修改编辑删除功能

--- gyw/Controllers/HomeController.cs
+++ gyw/Controllers/HomeController.cs
@@ -26,7 +26,7 @@ namespace gyw.Controllers
 
             return View();
         }
-        public ActionResult geng()
+        public ActionResult geng(int page=10)
         {
             string[] data = new string[] {"如何拯救被组织行乞的残疾人 ",
                                           "评论 | 清白自信的官员从不会躲企业家 ",
@@ -34,8 +34,29 @@ namespace gyw.Controllers
                                           "冯志明：冤案主导者的B面",
                                           "谁来救济输在智商起跑线上的儿童", };
             ViewBag.data = data;
+            ViewBag.Page = page;
+            return View();
+        }
+        public ActionResult Add()
+        {
+
 
             return View();
         }
+        public ActionResult Save(string title, string content)
+        {
+            ViewBag.TTitle = title;
+            ViewBag.Content = content;
+            return View();
+        }
+        public ActionResult Show(int id)
+        {
+            var db = new BlogDatabase();
+            var article = db.BlogArticles.First(o => o.Id == id);
+
+            ViewData.Model = article;
+            return View();
+        }
+
     }
 }
